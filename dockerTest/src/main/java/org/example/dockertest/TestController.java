115be8d65f25c009d20817dcb9c7b411b47c438d
ari <kjk9858@gmail.com>
2025-07-14 23:51:31 +0900

    도커 테스트

---
+++ dockerTest/src/main/java/org/example/dockertest/TestController.java
@@ -0,0 +1,13 @@
+package org.example.dockertest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/")
+    public String Test(){
+        return "ok";
+    }
+}
